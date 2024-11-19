5fc1217e2876bf936516bae6210ecd3a18a74679
KaelTorre <kaeltorre@gmail.com>
2024-11-19 08:42:58 -0500

    cuarto commit
    implementación de centralización de endpoints

--- ExamenParcial/src/main/java/tda2024/ExamenParcial/controller/AlumnoController.java
+++ ExamenParcial/src/main/java/tda2024/ExamenParcial/controller/AlumnoController.java
@@ -12,37 +12,38 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import tda2024.ExamenParcial.endpoints.AlumnosEndpoints;
 import tda2024.ExamenParcial.model.AlumnoModel;
 import tda2024.ExamenParcial.services.AlumnoService;
 
 @RestController
-@RequestMapping("alumno")
+@RequestMapping(AlumnosEndpoints.ALUMNO_ENDPOINT)
 public class AlumnoController {
     
     @Autowired
     AlumnoService alumnoService;
 
-    @GetMapping("/getAll")
+    @GetMapping(AlumnosEndpoints.ALUMNO_GET_ALL)
     public List<AlumnoModel> getAll(){
         return alumnoService.findAll();
     }
 
-    @PostMapping("/create")
+    @PostMapping(AlumnosEndpoints.ALUMNO_CREATE)
     public AlumnoModel create(@RequestBody AlumnoModel model){
         return alumnoService.add(model);
     }
 
-    @GetMapping("/getById/{id}")
+    @GetMapping(AlumnosEndpoints.ALUMNO_GET_BY_ID)
     public AlumnoModel getById(@PathVariable int id){
         return alumnoService.findById(id);
     }
     
-    @PutMapping("/update")
+    @PutMapping(AlumnosEndpoints.ALUMNO_UPDATE)
     public AlumnoModel update(@RequestBody AlumnoModel model){
         return alumnoService.update(model);
     }
 
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping(AlumnosEndpoints.ALUMNO_DELETE)
     public boolean delete(@PathVariable int id){
         return alumnoService.delete(id);
     }
---
+++ ExamenParcial/src/main/java/tda2024/ExamenParcial/endpoints/AlumnosEndpoints.java
@@ -0,0 +1,10 @@
+package tda2024.ExamenParcial.endpoints;
+
+public class AlumnosEndpoints {
+    public static final String ALUMNO_ENDPOINT = "alumno";
+    public static final String ALUMNO_CREATE = "/create";
+    public static final String ALUMNO_UPDATE = "/update";
+    public static final String ALUMNO_GET_ALL = "/getAll";
+    public static final String ALUMNO_GET_BY_ID = "/getById/{id}";
+    public static final String ALUMNO_DELETE = "/delete/{id}";
+}
